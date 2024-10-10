13f5b1722bbbd1cfc70460e74682c39aed3d703c
Igor Bastos <114706936+IgorBastos19@users.noreply.github.com>
2024-10-10 14:09:41 -0300

    metade do caminho da base seguimos estudando

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,37 @@
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+import database from "infra/database";
+
+export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+  const defaultMigrationOptions = {
+    dbClient: dbClient,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+  if (request.method === "GET") {
+    const pendingmigrations = await migrationRunner(defaultMigrationOptions);
+    await dbClient.end();
+    return response.status(200).json(pendingmigrations);
+  }
+
+  if (request.method === "POST") {
+    const migratedmigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false,
+    });
+
+    await dbClient.end();
+
+    if (migratedmigrations.length > 0) {
+      return response.status(201).json(migratedmigrations);
+    }
+
+    return response.status(200).json(migratedmigrations);
+  }
+
+  return response.status(405).end();
+}
